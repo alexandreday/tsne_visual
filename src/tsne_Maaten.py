77e48fd9704981aa9bbe511f435e0f5909ade5f1
Alexandre Day <alexandre.day1@gmail.com>
2016-12-23 15:58:14 -0500

    Python wrapper

---
+++ src/tsne_Maaten.py
@@ -0,0 +1,33 @@
+'''
+Created on Dec 23, 2016
+
+@author: Alexandre Day
+'''
+
+import numpy as np
+import pickle
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+def main():
+
+    print("Hello World")
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0
+
+
+
+
+
+    
+if __name__=="__main__":
+    main()
+
