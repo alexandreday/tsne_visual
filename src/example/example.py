dde0a7aec4d2ea276bafda2cf06d5e8adae44a91
Alexandre Day <alexandre.day1@gmail.com>
2017-01-16 15:25:14 -0500

    Removed, scaling

--- src/example/example.py
+++ src/example/example.py
@@ -7,10 +7,12 @@ from read_MNIST import load_mnist
 from sklearn.decomposition import PCA
 from sklearn import preprocessing as prep
 from matplotlib import pyplot as plt
+import latex
+import time
 
 np.set_printoptions(suppress=True)
 
-print("Running t-SNE on MNIST (with n=10000), then plotting the result")
+print("--> Running t-SNE on MNIST (with n=10000), then plotting the result")
 
 np.random.seed(0)
 x,y=load_mnist(path="MNIST/")
@@ -18,17 +20,19 @@ x,y=load_mnist(path="MNIST/")
 x=np.array(x[:10000],dtype=np.float)
 z=y[:10000]
 
-print("First scaling results and doing some PCA (n_components=40)")
-x_scale=prep.scale(x)
+print("--> First doing some PCA (n_components=40) to clear irrelevant dimensions")
 pca=PCA(n_components=40)
-xpca=pca.fit_transform(x_scale)
+xpca=pca.fit_transform(x)
 
-tsne=TSNE(n_components=2,early_exaggeration=12.0, learning_rate=200.0, n_iter=1000, 
+n_iter=1000
+tsne=TSNE(n_components=2,early_exaggeration=12.0, learning_rate=200.0, n_iter=n_iter, 
                  n_iter_lying=250,n_iter_momentum_switch=250
         )
- 
-print("Now let's run the t-SNE !\n\n")
+        
+print("--> Now let's run the t-SNE for %i iterations !\n\n"%n_iter)
+start=time.time() 
 xtsne=tsne.fit_transform(xpca)
+dt=time.time()-start
 
 print("Finally let's plot the results")
 palette=np.array(sns.color_palette('hls',10))
@@ -37,6 +41,7 @@ for i in range(10):
     pos=(z==i).flatten()
     plt.scatter(xtsne[pos,0],xtsne[pos,1],c=palette[i],label=str(i))
 
+plt.title('t-SNE with (Nsample=10000) and run for %i iterations\n Run time=%.2f s'%(n_iter,dt))
 plt.xlabel('t-SNE 1')
 plt.ylabel('t-SNE 2')
 plt.legend(loc='best')
