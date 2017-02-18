e9617c80887ad1039577bafc34df1e967cf7ba41
Alexandre Day <alexandre.day1@gmail.com>
2017-02-17 20:31:52 -0500

    Remove seaborn dependency

--- example/example.py
+++ example/example.py
@@ -1,5 +1,4 @@
 import numpy as np
-import seaborn as sns
 from tsne_visual.tsne import TSNE
 from read_MNIST import load_mnist
 from sklearn.decomposition import PCA
@@ -10,7 +9,7 @@ np.set_printoptions(suppress=True)
 
 print("--> Running t-SNE on MNIST (with n=10000), then plotting the result")
 
-np.random.seed(0)
+np.random.seed(0) # Always same seed here <--
 x,y=load_mnist(path="MNIST/")
 
 x=np.array(x[:10000],dtype=np.float)
@@ -20,11 +19,9 @@ print("--> First doing some PCA (n_components=40) to clear irrelevant dimensions
 pca=PCA(n_components=40)
 xpca=pca.fit_transform(x)
 
-n_iter=1000
-tsne=TSNE(n_components=2,early_exaggeration=12.0, learning_rate=200.0, n_iter=n_iter, 
-                 n_iter_lying=250,n_iter_momentum_switch=250
-        )
-        
+## Running t-SNE with default parameters !
+tsne=TSNE()
+## Otherwise use something like TSNE(perplexity=50, n_iter=2000, angle=0.3)
 print("--> Now let's run the t-SNE for %i iterations !\n\n"%n_iter)
 start=time.time() 
 xtsne=tsne.fit_transform(xpca)
@@ -37,10 +34,11 @@ for i in range(10):
     pos=(z==i).flatten()
     plt.scatter(xtsne[pos,0],xtsne[pos,1],c=palette[i],label=str(i))
 
-plt.title('t-SNE with (Nsample=10000) and run for %i iterations\n Run time=%.2f s'%(n_iter,dt))
+plt.title('t-SNE with (# of sample = 10000) and run for %i iterations\n run time=%.2f s'%(n_iter,dt))
 plt.xlabel('t-SNE 1')
 plt.ylabel('t-SNE 2')
 plt.legend(loc='best')
+plt.tight_layout()
 print('Voila !')
 plt.show()
 
